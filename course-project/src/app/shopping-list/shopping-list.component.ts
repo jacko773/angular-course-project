4a36683b7204e5f526a162bc21887a28569acf03
vinay <vinay18011@gmail.com>
2019-10-22 09:02:34 +0530

    shopping list dummy

--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/shared.module';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredient : Ingredient[] = [
+    new Ingredient('apple','5'),
+    new Ingredient('banana','7'),
+  ]
   constructor() { }
 
   ngOnInit() {
